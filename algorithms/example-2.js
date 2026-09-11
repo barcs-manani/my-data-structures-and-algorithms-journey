8bd1f9ba43f5161f2c2600ba8bc82bb707689d92
barcs-manani <berclaym@gmail.com>
2026-09-11 11:18:07 +0300

    fibonacci recursive

--- algorithms/example-2.js
+++ algorithms/example-2.js
@@ -15,16 +15,37 @@
 function fibonacciIterative(n) {
     const arr = [0, 1];
     for (let i = 2; i <= n; i++) {
-        arr.push(arr[i-1] + arr[i-2])
+        arr.push(arr[i - 1] + arr[i - 2])
     }
     return arr[n];
 }
 
 function fibonacciRecursive(n) {
-    if (n < 2)
-        return n;
-    return fibonacciRecursive(n-1) + fibonacciRecursive(n-2);
+        if (n < 2)
+            return n;
+        
+        return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
+}
+
+let calcs = 0;
+function fibonacciRecursiveMaster(n) {
+    let cache = {};
+    return function fib(n) {
+        calcs++;
+        if (cache[n])
+            return cache[n];
+        if (n < 2) {
+            cache[n] = n;
+            return n;
+        }
+        
+        cache[n] = fib(n - 1) + fib(n - 2);
+        return cache[n];
+    }
 }
 
 console.log("Iterative: ", fibonacciIterative(8));
 console.log("Recursive: ", fibonacciRecursive(8));
+const masterFib = fibonacciRecursiveMaster();
+console.log("Recursive Master: ", masterFib(8));
+console.log("Calcs: ", calcs);
